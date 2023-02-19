a5a68bb13bfeacf7ff146e8da9c04c975482714f
nirvana47 <nirvana47@gmail.com>
2023-02-19 14:52:38 +0000

    unnecessary header removed

--- DesignGurus/TargetSumEasy.java
+++ DesignGurus/TargetSumEasy.java
@@ -1,5 +1,3 @@
-import java.lang.annotation.Target;
-
 /*
  * Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target. 
  * Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
