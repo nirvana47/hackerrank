1dd8a6d44ddb4b9b9897d58a2e7c6b5c281b6899
nirvana47 <nirvana47@gmail.com>
2023-02-23 04:25:05 +0000

    In an array find all unique triplets that add to 0

---
+++ DesignGurus/TripletSumToZero.java
@@ -0,0 +1,5 @@
+public class TripletSumToZero {
+    public static void main(String[] args) {
+        System.out.println("Triplet sum to Zero: Given an array of unsorted numbers, find all unique triplets in it that add up to zero.");
+    }
+}
